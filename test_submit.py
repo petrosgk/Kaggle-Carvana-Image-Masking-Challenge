c14068dcb2df3d3f4eb7a144715b176c9d836261
Peter Giannakopoulos <petrosgk@users.noreply.github.com>
2017-08-10 15:48:50 +0300

    Update test_submit.py

--- test_submit.py
+++ test_submit.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-from model.u_net import get_unet_128, get_unet_256, get_unet_512
+from model.u_net import get_unet_128, get_unet_256, get_unet_512, get_unet_1024
 
 df_test = pd.read_csv('input/sample_submission.csv')
 ids_test = df_test['img'].map(lambda s: s.split('.')[0])
@@ -39,35 +39,19 @@ def run_length_encode(mask):
 
 rles = []
 
-test_splits = 8  # Split test set (number of splits must be multiple of 2)
-ids_test_splits = np.split(ids_test, indices_or_sections=test_splits)
-
-split_count = 0
-for ids_test_split in ids_test_splits:
-    split_count += 1
-
-
-    def test_generator():
-        while True:
-            for start in range(0, len(ids_test_split), batch_size):
-                x_batch = []
-                end = min(start + batch_size, len(ids_test_split))
-                ids_test_split_batch = ids_test_split[start:end]
-                for id in ids_test_split_batch.values:
-                    img = cv2.imread('input/test/{}.jpg'.format(id))
-                    img = cv2.resize(img, (input_size, input_size))
-                    x_batch.append(img)
-                x_batch = np.array(x_batch, np.float32) / 255
-                yield x_batch
-
-
-    print("Predicting on {} samples (split {}/{})".format(len(ids_test_split), split_count, test_splits))
-    preds = model.predict_generator(generator=test_generator(),
-                                    steps=np.ceil(float(len(ids_test_split)) / float(batch_size)))
+print('Predicting on {} samples with batch_size = {}...'.format(len(ids_test), batch_size))
+for start in tqdm(range(0, len(ids_test), batch_size)):
+    x_batch = []
+    end = min(start + batch_size, len(ids_test))
+    ids_test_batch = ids_test[start:end]
+    for id in ids_test_batch.values:
+        img = cv2.imread('input/test/{}.jpg'.format(id))
+        img = cv2.resize(img, (input_size, input_size))
+        x_batch.append(img)
+    x_batch = np.array(x_batch, np.float32) / 255
+    preds = model.predict_on_batch(x_batch)
     preds = np.squeeze(preds, axis=3)
-
-    print("Generating masks...")
-    for pred in tqdm(preds, miniters=1000):
+    for pred in preds:
         prob = cv2.resize(pred, (orig_width, orig_height))
         mask = prob > threshold
         rle = run_length_encode(mask)
