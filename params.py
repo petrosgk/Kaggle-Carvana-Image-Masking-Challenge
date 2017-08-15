c6f90966b259e5b8d394a3aa6249732c04a9d77f
Peter Giannakopoulos <petrosgk@users.noreply.github.com>
2017-08-15 23:48:24 +0300

    Create params.py

---
+++ params.py
@@ -0,0 +1,13 @@
+from model.u_net import get_unet_128, get_unet_256, get_unet_512, get_unet_1024
+
+input_size = 128
+
+max_epochs = 50
+batch_size = 16
+
+orig_width = 1918
+orig_height = 1280
+
+threshold = 0.5
+
+model = get_unet_128()
