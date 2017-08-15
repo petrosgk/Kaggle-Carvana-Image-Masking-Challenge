714a84fdd075dbaf0c5baaf64fe8ca70eb759a13
Peter Giannakopoulos <petrosgk@users.noreply.github.com>
2017-08-15 23:47:38 +0300

    Create losses.py

---
+++ model/losses.py
@@ -0,0 +1,14 @@
+from keras.losses import binary_crossentropy
+import keras.backend as K
+
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + (1 - dice_loss(y_true, y_pred))
