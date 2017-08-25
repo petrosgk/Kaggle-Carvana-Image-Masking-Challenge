706bc3781a445bcf89123aacd2affdf4a9c50af8
Peter Giannakopoulos <petrosgk@users.noreply.github.com>
2017-08-25 09:32:40 +0300

    Fixed naming

--- model/losses.py
+++ model/losses.py
@@ -2,7 +2,7 @@ from keras.losses import binary_crossentropy
 import keras.backend as K
 
 
-def dice_loss(y_true, y_pred):
+def dice_coeff(y_true, y_pred):
     smooth = 1.
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
@@ -10,5 +10,9 @@ def dice_loss(y_true, y_pred):
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
 
+def dice_loss(y_true, y_pred):
+    return 1 - dice_coeff(y_true, y_pred)
+
+
 def bce_dice_loss(y_true, y_pred):
-    return binary_crossentropy(y_true, y_pred) + (1 - dice_loss(y_true, y_pred))
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
